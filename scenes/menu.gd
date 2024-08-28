0325f5c88447fe1d1397c009ed9cf1217338b3fc
Matt Giuca <matt.giuca@gmail.com>
2024-08-29 00:47:57 +1000

    Handle touch events in the menu.
    This is a horrible hack (manually handling gui_input on each button). I
    have no idea why the buttons don't just respond to touch events.

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -23,3 +23,34 @@ func _on_btn_restart_pressed() -> void:
 
 func _on_btn_quit_pressed() -> void:
   get_tree().quit()
+
+func in_bounds(control: Control, position: Vector2):
+  var bound_rect : Rect2 = Rect2(Vector2.ZERO, control.get_size())
+  return bound_rect.has_point(position)
+
+# Used to detect if a touch event is an "up" event within a control, implying
+# that the control was pressed like a button from a touch event.
+func event_touch_in_bounds(control: Control, event: InputEvent):
+  return event is InputEventScreenTouch and not event.pressed and \
+         in_bounds(control, event.position)
+
+# Manually detect touch events on buttons and enact them.
+# WTF: I should not have to do this. It's very weird that buttons do not respond
+# to touch events at all and it's my responsibility to write this logic...??
+
+func _on_btn_resume_gui_input(event: InputEvent) -> void:
+  if event_touch_in_bounds($MarginContainer/VBoxContainer/BtnResume, event):
+    _on_btn_resume_pressed()
+
+func _on_chk_fullscreen_gui_input(event: InputEvent) -> void:
+  var chk = $MarginContainer/VBoxContainer/ChkFullscreen
+  if event_touch_in_bounds(chk, event):
+    chk.button_pressed = not chk.button_pressed
+
+func _on_btn_restart_gui_input(event: InputEvent) -> void:
+  if event_touch_in_bounds($MarginContainer/VBoxContainer/BtnRestart, event):
+    _on_btn_restart_pressed()
+
+func _on_btn_quit_gui_input(event: InputEvent) -> void:
+  if event_touch_in_bounds($MarginContainer/VBoxContainer/BtnQuit, event):
+    _on_btn_quit_pressed()
